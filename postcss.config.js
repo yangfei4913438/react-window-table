600a751d2762dc2a22efdccbb73fb69399acbc0f
杨飞 <yangfei86@vip.qq.com>
2022-10-11 15:46:41 +0800

    基础配置更新

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
+    // postcss-nested 使用 Sass 语法。postcss-nesting使用 CSS 嵌套规范草案中的语法。
+    'tailwindcss/nesting': 'postcss-nested',
     tailwindcss: {},
     autoprefixer: {
       flexbox: 'no-2009',
