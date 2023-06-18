b806005ed620426580f0b488d7f9a1b9165109ed
Fei Yang (杨飞) <yangfei86@vip.qq.com>
2023-06-19 03:30:57 +0800

    :sparkles: feat: 行选中，默认显示行号，hover之后显示checkbox (#6)

--- lib/IndeterminateCheckbox.tsx
+++ lib/IndeterminateCheckbox.tsx
@@ -1,10 +1,11 @@
 import { Checkbox } from 'antd';
 import cx from 'classnames';
-import React, { FC, useContext, useMemo } from 'react';
+import React, { FC, useContext, useMemo, useState } from 'react';
 
 import { checkBoxWidth, dragIconWidth, VirtualTableContext } from './consts';
 
 interface IndeterminateCheckboxProps {
+  order?: number;
   indeterminate: boolean;
   checked: boolean;
   onClick: () => void;
@@ -12,12 +13,14 @@ interface IndeterminateCheckboxProps {
 }
 
 const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
+  order = 0,
   indeterminate = false,
   checked = false,
   className = '',
   onClick,
 }) => {
-  const { canDragSortRow, fixedLeftCount } = useContext(VirtualTableContext);
+  const { canDragSortRow, fixedLeftCount, canChecked } = useContext(VirtualTableContext);
+  const [hover, setHover] = useState(false);
 
   const leftStyle = useMemo(() => {
     if (fixedLeftCount === 0) {
@@ -28,6 +31,13 @@ const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
     return {};
   }, [fixedLeftCount]);
 
+  const RenderNumber = () => {
+    if (order > 0) {
+      return <div className='text-gray-700'>{order}</div>;
+    }
+    return null;
+  };
+
   return (
     <div
       role='cell'
@@ -35,6 +45,8 @@ const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
         'sticky left-0 z-2 flex h-full items-center justify-center bg-white',
         className
       )}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
       style={{
         minWidth: checkBoxWidth,
         left: canDragSortRow ? dragIconWidth : 0,
@@ -43,10 +55,14 @@ const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
       onClick={(e) => {
         // 阻止冒泡，避免触发行点击事件。
         e.stopPropagation();
+        // 判断是否能点击
+        if (!canChecked) return;
         // 执行回掉方法
         onClick();
       }}
       onKeyPress={(e) => {
+        // 判断是否能点击
+        if (!canChecked) return;
         // 空格键响应
         if (e.code === 'Space') {
           // 阻止冒泡，避免触发行点击事件。
@@ -56,6 +72,8 @@ const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
         }
       }}
       onKeyDown={(e) => {
+        // 判断是否能点击
+        if (!canChecked) return;
         // 回车响应
         if (e.key === 'Enter') {
           // 阻止冒泡，避免触发行点击事件。
@@ -65,7 +83,11 @@ const IndeterminateCheckbox: FC<IndeterminateCheckboxProps> = ({
         }
       }}
     >
-      <Checkbox indeterminate={indeterminate} checked={checked} />
+      {canChecked && (hover || checked || indeterminate) ? (
+        <Checkbox indeterminate={indeterminate} checked={checked} />
+      ) : (
+        <RenderNumber />
+      )}
     </div>
   );
 };
